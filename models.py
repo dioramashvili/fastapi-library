948b24aedebab8ddf49b2dcc0908033234f8a81e
Davit Ioramashvili <dioramashvili@gmail.com>
2025-05-30 23:12:30 +0400

    Refactor Book model to inherit from BookCreate and add validation for year field

--- models.py
+++ models.py
@@ -1,18 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 from uuid import UUID
-
-
-class Book(BaseModel):
-    id: UUID
-    title: str
-    author: str
-    year: int
-    genre: Optional[str] = None
 
 
 class BookCreate(BaseModel):
     title: str
     author: str
-    year: int
+    year: int = Field(..., gt=0, lt=2100)
     genre: Optional[str] = None
+
+
+class Book(BookCreate):
+    id: UUID
